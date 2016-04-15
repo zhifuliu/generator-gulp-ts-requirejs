8ed1c79f082f76fdf61bdec73138de696d1ba83b
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-04-15 18:55:28 +0800

    初始化 angular 分支

---
+++ src/app/startup.ts
@@ -0,0 +1,21 @@
+/// <reference path="../references.d.ts" />
+
+import $ = require("jquery");
+
+class Application {
+    constructor() {
+
+    }
+    public clickNum: number = 0;
+    public clickAddOne = () => {
+        $('#msg').val((this.clickNum++).toString());
+        console.log(this.clickNum)
+    }
+    public clickMenu = menuName => {
+        console.log(menuName);
+    }
+}
+
+var app = new Application();
+ko.applyBindings(app);
+export = app;
